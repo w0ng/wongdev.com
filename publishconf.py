fa74b72c4087faa2c9495e3503197ee325fd21a2
Andrew Wong <akamchoiw@gmail.com>
2014-01-06 23:51:16 +1100

    Integrate with ant-build-script to optimise files when publishing

--- publishconf.py
+++ publishconf.py
@@ -16,7 +16,7 @@ RELATIVE_URLS = False
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = None
 
-#DELETE_OUTPUT_DIRECTORY = True
+DELETE_OUTPUT_DIRECTORY = True
 
 # Following items are often useful when publishing
 
