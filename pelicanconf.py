2caecaa685efda590212c39defab0c5abb85992b
Andrew Wong <akamchoiw@gmail.com>
2014-01-11 08:35:19 +1100

    Remove categories and authors pages

--- pelicanconf.py
+++ pelicanconf.py
@@ -16,14 +16,22 @@ ARTICLE_LANG_SAVE_AS = 'blog/{slug}-{lang}/index.html'
 ARTICLE_LANG_URL = 'blog/{slug}-{lang}/'
 ARTICLE_SAVE_AS = 'blog/{slug}/index.html'
 ARTICLE_URL = 'blog/{slug}/'
-AUTHORS_SAVE_AS = 'blog/authors/index.html'
-AUTHORS_URL = 'blog/authors/'
-AUTHOR_SAVE_AS = 'blog/authors/{slug}/index.html'
-AUTHOR_URL = 'blog/authors/{slug}/'
-CATEGORIES_SAVE_AS = 'blog/categories/index.html'
-CATEGORIES_URL = 'blog/categories/'
-CATEGORY_SAVE_AS = 'blog/categories/{slug}/index.html'
-CATEGORY_URL = 'blog/categories/{slug}/'
+#AUTHORS_SAVE_AS = 'blog/authors/index.html'
+#AUTHORS_URL = 'blog/authors/'
+#AUTHOR_SAVE_AS = 'blog/authors/{slug}/index.html'
+#AUTHOR_URL = 'blog/authors/{slug}/'
+#CATEGORIES_SAVE_AS = 'blog/categories/index.html'
+#CATEGORIES_URL = 'blog/categories/'
+#CATEGORY_SAVE_AS = 'blog/categories/{slug}/index.html'
+#CATEGORY_URL = 'blog/categories/{slug}/'
+AUTHORS_SAVE_AS = ''
+AUTHORS_URL = ''
+AUTHOR_SAVE_AS = ''
+AUTHOR_URL = ''
+CATEGORIES_SAVE_AS = ''
+CATEGORIES_URL = ''
+CATEGORY_SAVE_AS = ''
+CATEGORY_URL = ''
 TAGS_SAVE_AS = 'blog/tags/index.html'
 TAGS_URL = 'blog/tags/'
 TAG_SAVE_AS = 'blog/tags/{slug}/index.html'
@@ -58,3 +66,6 @@ EXTRA_PATH_METADATA = {
 MENUITEMS = (
         ('RSS','http://wongdev.com/feeds/all.atom.xml'),
 )
+
+#DIRECT_TEMPLATES = (('index', 'tags', 'categories', 'authors', 'archives'))
+DIRECT_TEMPLATES = (('index', 'tags', 'archives'))
